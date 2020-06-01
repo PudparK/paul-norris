9cad34ae7006574c2cae5cf212ce2a542fc5d736
Paul Barron <p.barron@outlook.com>
2020-05-31 19:52:38 -0500

    Updates

---
+++ precache-manifest.2bfe9ff6dbb77585980026e4d0c02d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a549c06c354811ab1ca6169245f5e141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "353c5281bd7f92648703",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4d467665b601d2015ab",
+    "url": "/static/js/2.85efb4da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85efb4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353c5281bd7f92648703",
+    "url": "/static/js/main.40fe077c.chunk.js"
+  },
+  {
+    "revision": "649aeb8ee90e2bfb3e34",
+    "url": "/static/js/runtime-main.0efbee12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
